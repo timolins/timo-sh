55a5ef56ffa7dfd2146c64e5bbcaa69f5eb606fe
Timo Lins <me@timo.sh>
2020-08-06 23:23:15 +0200

    Change slogan

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -8,23 +8,23 @@ export const Hero: React.FC = () => (
       <div className="flex flex-col md:items-center my-16">
         <h1 className="text-4xl font-bold">Hey, I'm Timo!</h1>
         <p className="text-2xl text-gray-700">
-          I'm passionate about JavaScript, Design and Film Making.
+          I design and build apps, and everything that comes with that.
         </p>
         <div className="max-w-sm md:mx-auto my-8 -ml-2">
           <a
-            className="mx-1 md:mx-2 py-1 px-2 rounded bg-blue-200 hover:bg-blue-300 text-blue-700"
+            className="mx-1 md:mx-2 py-1 px-2 rounded bg-blue-200 hover:bg-blue-300 text-blue-800"
             href="https://twitter.com/timolins"
           >
             Twitter
           </a>
           <a
-            className="mx-1 md:mx-2 py-1 px-2 rounded bg-gray-300 hover:bg-gray-400 text-gray-700"
+            className="mx-1 md:mx-2 py-1 px-2 rounded bg-gray-300 hover:bg-gray-400 text-gray-800"
             href="https://github.com/timolins"
           >
             GitHub
           </a>
           <a
-            className="mx-1 md:mx-2 py-1 px-2 rounded bg-pink-200 hover:bg-pink-300 text-pink-700"
+            className="mx-1 md:mx-2 py-1 px-2 rounded bg-pink-200 hover:bg-pink-300 text-pink-800"
             href="https://dribbble.com/timolins"
           >
             Dribbble
--- components/sections/nav.tsx
+++ components/sections/nav.tsx
@@ -16,7 +16,7 @@ export const Nav: React.FC = () => (
       </li> */}
       <li>
         <Link href="/work">
-          <a className="text-blue-600 mr-2 px-2 py-1 rounded-md">Work</a>
+          <a className="text-blue-700 mr-2 px-2 py-1 rounded-md">Work</a>
         </Link>
       </li>
     </ul>
